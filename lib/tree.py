87f167482fffe30949abd55bc07a0279117f13d7
Artem Raskin <artemraskin@Artems-MacBook-Pro.local>
2025-09-29 14:56:02 -0700

    Set up the method which search through tree by index

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        stack = [self.root]
+
+        while stack:
+            node = stack.pop()
+            if node['id'] == id:
+                return node
+            stack.extend(node.get('children', []))
+
+        return None
+    
